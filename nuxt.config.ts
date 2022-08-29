8473b6650a24a53a7447a8826d5541b09b0c9f3d
tobomuto <thomas@ylly.fr>
2022-08-29 12:03:18 +0200

    tailwind + sass install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    modules: ['@nuxtjs/tailwindcss'],
+    css: [
+        '~/assets/styles/_variables.scss'
+    ],
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: 'tailwind.config.js',
+        exposeConfig: false,
+        config: {},
+        injectPosition: 'first',
+        viewer: true,
+    }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>blep</h1>
+  <h1>Test</h1>
 </template>
 
 <script>
