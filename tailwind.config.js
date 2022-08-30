4751f922157c79ff9c5e1de6930c5895e9c5e736
tobomuto <thomas@ylly.fr>
2022-08-30 12:49:43 +0200

    tailwind + scss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      'white': '#FFFFFF',
+      'bodyBackground': '#F5F6FA',
+      'darkGrayBackground': '#19353E',
+      'textMainColor': '#19353E',
+      'primary': '#27B3B8',
+      'secondary': '#009892',
+      'tertiary': '#F4704D',
+      'warning': '#F39131',
+      'borderColor': '#CCEAE9',
+      'warning': '#F39131',
+      'creatingStatus': '#CCEAE9',
+      'createdStatus': '#27B3B8',
+      'modificatingStatus': '#F3C431',
+      'archivedStatus': '#DB4943',
+    },
+    fontFamily: {
+      regular: ['Poppins-regular', 'sans-serif'],
+      bold: ['Poppins-bold', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
