5e30f36d3473a5df73436825b2d88ee9cf0bc5bc
tobomuto <thomas@ylly.fr>
2022-09-01 10:42:16 +0200

    listing layout with navABC component

---
+++ components/Header/Alphabet.vue
@@ -0,0 +1,52 @@
+<template>
+    <header>
+        <ul>
+            <li
+            v-for="(letter, index) in alphabet"
+            :key="index"
+            >
+            <span>{{  letter }}</span>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            alphabet: "abcdefghijklmnopqrstuvwxyz".split("")
+        }
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+
+@import '~/assets/styles/_variables.scss';
+
+header {
+    background-color: $darkGrayBackground;
+    ul {
+        max-width: 1140px;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: repeat(26, auto);
+        li {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 37px;
+            color: white;
+            font-size: 12px;
+            background-color: $darkGrayBackground;
+            text-transform: uppercase;
+            cursor: pointer;
+            &:hover {
+                background-color: $primary;
+            }
+        }
+    }
+}
+
+</style>
---
+++ layouts/listing.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <HeaderNavbar/>
+    <HeaderAlphabet/>
+    <main>
+        <slot/>
+    </main>
+
+    <Footer/>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
